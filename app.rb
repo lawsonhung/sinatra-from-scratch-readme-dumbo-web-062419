14f12197a64122d3eec70a03ee6571da1cd8eba4
github username <github email address>
2019-07-14 23:03:34 -0400

    Done

--- app.rb
+++ app.rb
@@ -1,8 +1,11 @@
 require 'sinatra'
 class App < Sinatra::Base
 
-  get '/' do 
-    "Hello, world!"
+  # get '/' do 
+  #   "Hello, world!"
+  # end
+  get '/badURL' do
+    "Hello World"
   end
 
 end
